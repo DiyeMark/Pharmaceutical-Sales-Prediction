433bb77f8d1b5933d32e62cc4c4a0ba197a3d546
Diye Mark <diyye101@gmail.com>
2022-05-24 09:00:05 +0300

    setup: dvc, cml, mlflow, requirements.txt, setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Pharmaceutical-Sales-Prediction',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/DiyeMark/Pharmaceutical-Sales-Prediction',
+    license='',
+    author='DiyeMark',
+    author_email='diyye101@gmail.com',
+    description='Python Implementation of Rossmann Store Sales Predication'
+)
