bf2e418e9b9f4769edbc8e029442cfd1591ef463
Diye Mark <diyye101@gmail.com>
2022-05-28 22:22:47 +0300

    modified streamlit dashboard

--- streamlit_dashboard/data_exploration_part_I.py
+++ streamlit_dashboard/data_exploration_part_I.py
@@ -1,2 +1,48 @@
+import streamlit as st
+import pandas as pd
+
+
 def app():
-    pass
+    st.title("Exploratory Data Analysis Part I")
+
+    st.subheader("Top 6 features with highest correlation with sales")
+    st.image('./images/correlation_map.png')
+
+    st.subheader("State Holidays")
+    st.image('./images/state_holiday.png')
+
+    st.subheader("Sales During State Holidays")
+    st.image('./images/sales_during_state_holiday.png')
+
+    st.subheader("Customers During State Holidays")
+    st.image('./images/customers_during_holidays.png')
+
+    st.subheader("Store Type")
+    st.image('./images/store_type_count.png')
+
+    st.subheader("Sales and Customers Across Store Type")
+    st.image('./images/customers_across_store_type.png')
+
+    st.subheader("Assortment Type")
+    st.image('./images/assortment_type_count.png')
+
+    st.subheader("Sales and Customers Across Assortment Type")
+    st.image('./images/customers_across_assortment.png')
+
+    st.subheader("Store Open Status Across Different Days of the Week")
+    st.image('./images/store_open.png')
+
+    st.subheader("Sales and Customers Across Different Days of the Week")
+    st.image('./images/customers_across_different_days.png')
+
+    st.subheader("Weekday")
+    st.image('./images/weekday.png')
+
+    st.subheader("Sales and Customers During Weekends vs Weekdays")
+    st.image('./images/customers_during_weekend.png')
+
+    st.subheader("Promo")
+    st.image('./images/promo_count.png')
+
+    st.subheader("Sales and Customers Across Different Promo")
+    st.image('./images/customer_across_promo.png')
--- streamlit_dashboard/data_exploration_part_II.py
+++ streamlit_dashboard/data_exploration_part_II.py
@@ -1,2 +1,35 @@
+import streamlit as st
+import pandas as pd
+
+
 def app():
-    pass
+    st.subheader("Mean Sales in the 5 Competition Distance Decile Classes")
+    st.image('./images/mean_sales_in_decile_classess.png')
+
+    st.subheader("Competition Distance")
+    st.image('./images/competition_distance.png')
+
+    st.subheader("Seasonality Averaged Daily")
+    st.image('./images/seasonality_plot.png')
+
+    st.subheader("Seasonality Averaged Weekly")
+    st.image('./images/seasonality_plot_weekly.png')
+
+    st.subheader("Seasonality Averaged Monthly")
+    st.image('./images/seasonality_plot_monthly.png')
+
+    st.subheader("Seasonality Averaged Year")
+    st.image('./images/seasonality_plot_yearly.png')
+
+    st.subheader("Sales Distribution")
+    st.image('./images/sales_distribution.png')
+
+    st.subheader("Sales")
+    st.image('./images/sales_box_plot.png')
+
+    st.subheader("Customer")
+    st.image('./images/customers_box_plot.png')
+
+    st.subheader("Closest Competition")
+    st.image('./images/closest_competition_box_plot.png')
+
